cf7674470d76b64f8b172b5cb06afe64b51b579b
Marco943 <80267881+Marco943@users.noreply.github.com>
2022-04-18 15:20:33 -0300

    Add files via upload

---
+++ Raio-X Streamlit.py
@@ -0,0 +1,89 @@
+# Importar Streamlit e Pandas
+import streamlit as st, pandas as pd
+# Importar função de baixar arquivo do sharepoint
+from funcoes import baixar_excel_sharepoint, subir_arquivo_sharepoint
+from datetime import datetime
+
+# Configura o Título da página
+st.set_page_config(page_title='Raio-X Piloto')
+
+# Puxa Login e Senha do arquivo secreto
+login = st.secrets['login_microsoft']['login']
+senha = st.secrets['login_microsoft']['senha']
+url_sharepoint = 'https://sucessoemvendassv.sharepoint.com'
+time = time = '/sites/GestodeResultados'
+caminho_arquivo = '/Shared Documents/10- Desenvolvimento/Streamlit/Raio-X/Perguntas Raio-x.xlsx'
+caminho_respostas = '/Shared Documents/10- Desenvolvimento/Streamlit/Raio-X/Respostas/'
+
+# Função que puxa o arquivo do sharepoint e mantém em cache
+@st.cache
+def perguntas():
+    arquivo_baixado = baixar_excel_sharepoint(url_sharepoint, time, caminho_arquivo, login, senha)
+    return arquivo_baixado
+# Guarda o arquivo do sharepoint na variável perguntas
+perguntas = perguntas()
+
+# Definição dos elementos da página
+cabecalho = st.container()
+formulario = st.container()
+enviar = st.button('Enviar')
+
+# Preencher cabeçalho
+with cabecalho:
+    st.header('App Piloto Raio-X')
+
+# Preencher Formulário
+with formulario:
+    # Inicializa um dicionário para guardar as respostas
+    respostas = dict()
+
+    # Faz um loop em cada etapa que existe no raio-x, normalmente 6
+    for etapa in perguntas['ID_Etapa'].unique():
+        # Faz uma tabela filtrada só para a etapa atual
+        perguntas_f = perguntas[perguntas['ID_Etapa'] == etapa]
+        # Pega o nome da etapa atual
+        nome_etapa = perguntas_f['Etapa'].max()
+
+        # Para a etapa atual, cria um expander com o nome da etapa
+        with st.expander(label=nome_etapa):
+
+            # Se for a última etapa, faz um botão de rádio para perguntar se o cliente fechou a compra
+            if etapa == perguntas['ID_Etapa'].unique()[-1]:
+                fechoucompra = st.radio(
+                    label = 'O cliente fechou a compra?',
+                    options = [True, False], index = 0, # O índice diz qual a seleção padrão
+                    format_func = lambda x: {False: 'Não', True: 'Sim'}.get(x)
+                )
+
+            # Para a etapa atual, faz um loop em cada pergunta que existe nesta etapa
+            for pergunta in perguntas_f['ID_Pergunta'].unique():
+
+                # Faz uma tabela filtrada só com a pergunta atual, ou seja, tabela de uma linha
+                perguntas_ff = perguntas_f[perguntas_f['ID_Pergunta'] == pergunta]
+
+                # Pega a descrição da pergunta atual
+                nome_pergunta = perguntas_ff['Pergunta'].max()
+
+                # Verifica se a pergunta é dependente ou não de o cliente ter fechado a compra, e também verifica se a opção de o cliente ter fechado a compra está como Sim ou Não. Vai mostrar só as perguntas correspondentes
+                if (perguntas_ff['Compra_Fechada'].max() == 0) or (perguntas_ff['Compra_Fechada'].max() == -1 and fechoucompra == False) or (perguntas_ff['Compra_Fechada'].max() == 1 and fechoucompra == True):
+
+                    # Guarda nas respostas um True ou False, com o valor do ID da pergunta. Ex: {1: True, 2: False}
+                    respostas[int(pergunta)] = st.checkbox(
+                        label = nome_pergunta,
+                        key = {'pergunta_{}'.format(pergunta)}
+                    )
+                # Se a pergunta não deve ser mostrada, continua o loop sem fazer nada
+                else:
+                    continue
+
+
+#st.dataframe(respostas)
+#perguntas['Fechou'] = fechoucompra
+#perguntas['ID_Pergunta']
+# respostas = pd.DataFrame()
+if enviar:
+    respostas = pd.DataFrame.from_dict(respostas, orient='index').reset_index().rename(columns = {'index': 'ID_Pergunta', 0: 'Resposta'})
+    respostas['Fechou'] = fechoucompra
+    datahora = datetime.now().strftime('%d_%m_%Y_%H_%M_%S')
+    nome_arquivo = 'Resposta_Raio-X_{}.csv'.format(datahora)
+    subir_arquivo_sharepoint(respostas, url_sharepoint, time, caminho_respostas+nome_arquivo, login, senha)
---
+++ funcoes.py
@@ -0,0 +1,43 @@
+import pandas as pd, io
+from office365.runtime.auth.user_credential import UserCredential
+from office365.sharepoint.client_context import ClientContext
+from office365.sharepoint.files.file import File
+
+def baixar_excel_sharepoint(url_sharepoint, time, caminho_arquivo, login, senha):
+    url = url_sharepoint #'https://sucessoemvendassv.sharepoint.com'
+    time = time #'/sites/GestodeResultados'
+    login = login #'marco@sucessoemvendas.com.br'
+    senha = senha #'Mac30054!'
+    caminho_arquivo = caminho_arquivo #'/Shared Documents/10- Desenvolvimento/Streamlit/Raio-X/Perguntas Raio-X.xlsx'
+
+    # Conectar ao site com as credenciais
+    ctx = ClientContext(url+time).with_credentials(UserCredential(login, senha))
+
+    # Checar se está conectado ao site
+    # web = ctx.web
+    # ctx.load(web)
+    # ctx.execute_query()
+    # print("Site title: {0}".format(web.properties['Title']))
+
+    response = File.open_binary(ctx, time+caminho_arquivo)
+
+    bytes_file_obj = io.BytesIO()
+    bytes_file_obj.write(response.content)
+    bytes_file_obj.seek(0)
+    return pd.read_excel(bytes_file_obj)
+
+def subir_arquivo_sharepoint(dados, url_sharepoint, time, caminho_arquivo, login, senha):
+    url = url_sharepoint #'https://sucessoemvendassv.sharepoint.com'
+    time = time #'/sites/GestodeResultados'
+    login = login #'marco@sucessoemvendas.com.br'
+    senha = senha #'Mac30054!'
+    caminho_arquivo = caminho_arquivo[1:] if caminho_arquivo[0] == '/' else caminho_arquivo #'/Shared Documents/10- Desenvolvimento/Streamlit/Raio-X/Perguntas Raio-X.xlsx'
+
+    ctx = ClientContext(url+time).with_credentials(UserCredential(login, senha))
+
+    buffer = io.BytesIO()
+    dados.to_csv(buffer, index=False, encoding = 'utf-8')
+    buffer.seek(0)
+    file_content = buffer.read()
+
+    ctx.web.get_folder_by_server_relative_url(time).upload_file(caminho_arquivo, file_content).execute_query()
