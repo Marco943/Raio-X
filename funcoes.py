22f51051ab38fadcbe3efed30820fe2a2a9a8d1f
Marco943 <80267881+Marco943@users.noreply.github.com>
2022-04-26 14:05:07 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -4,10 +4,10 @@ from office365.sharepoint.client_context import ClientContext
 from office365.sharepoint.files.file import File
 
 def baixar_excel_sharepoint(url_sharepoint, time, caminho_arquivo, login, senha):
-    url = url_sharepoint #'https://sucessoemvendassv.sharepoint.com'
-    time = time #'/sites/GestodeResultados'
-    login = login #'marco@sucessoemvendas.com.br'
-    senha = senha #'Mac30054!'
+    url = url_sharepoint
+    time = time
+    login = login
+    senha = senha
     caminho_arquivo = caminho_arquivo #'/Shared Documents/10- Desenvolvimento/Streamlit/Raio-X/Perguntas Raio-X.xlsx'
 
     # Conectar ao site com as credenciais
